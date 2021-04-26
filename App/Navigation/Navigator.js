fd8bc7b63d63ec43d5fdd8ff3184baf345379e3c
Griffin Sullivan <griffinsully@gmail.com>
2021-04-26 17:27:06 -0400

    adding icons to navigation

--- App/Navigation/Navigator.js
+++ App/Navigation/Navigator.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 import Chat from '../Screens/Chat';
 import Symptoms from '../Screens/Symptoms'; 
 import Support from '../Screens/Support';
@@ -12,7 +13,29 @@ const Tab = createBottomTabNavigator();
 export default function Navigator() {
     return (
         <NavigationContainer>
-            <Tab.Navigator> 
+            <Tab.Navigator
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ color, size }) => {
+                        let iconName;
+
+                        if (route.name === 'Chat') {
+                            iconName = "chatbox-ellipses"
+                        } else if (route.name === 'Journal') {
+                            iconName = "journal"
+                        } else if (route.name === 'Manage Symptoms') {
+                            iconName = "thermometer"
+                        } else if (route.name === 'Support') {
+                            iconName = "ios-help-buoy"
+                        }
+                        return <Ionicons name={iconName} size={size} color={color} />
+                    }
+                })}
+                tabBarOptions={{
+                    labelStyle: { fontSize: 10 },
+                    activeTintColor: '#03c2fc',
+                    style: { height: '8%', paddingTop: '1%', paddingBottom: '1%' }
+                }}
+            > 
                 <Tab.Screen name="Chat" component={Chat}/>
                 <Tab.Screen name="Journal" component={Journal} />
                 <Tab.Screen name="Manage Symptoms" component={Symptoms}/>
